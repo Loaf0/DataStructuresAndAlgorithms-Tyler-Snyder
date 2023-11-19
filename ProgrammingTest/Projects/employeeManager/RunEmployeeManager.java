087e0b8558dc51e16608f43c2d8fb867b3efd04e
ninja <ninja@Telefax-2840>
2023-11-19 12:04:33 -0500

    removed temp text

--- ProgrammingTest/Projects/employeeManager/RunEmployeeManager.java
+++ ProgrammingTest/Projects/employeeManager/RunEmployeeManager.java
@@ -4,21 +4,6 @@ public class RunEmployeeManager
 {
 	public static void main(String[] args) 
 	{
-		/*
-		Department dept = new Department("test Department");
-		dept.addEmployee(new Employee("Bob"));
-		dept.addEmployee(new Employee("Cob"));
-		dept.addEmployee(new Employee("Lob"));
-		
-//		dept.showPay("Bob");
-//		dept.showPay("Cob");
-//		dept.showPay("Lob");
-		
-		dept.payroll();
-		
-		dept.addEmployee(dept.removeEmployee("Cob"));
-		System.out.println(dept.contains("Cob"));
-		dept.payroll()*/
 		EmployeeManager em = new EmployeeManager();
 		em.runManager();
 	}
