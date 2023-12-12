6d95820bd765c8a772a515994e6176716ec2c19e
ninja <ninja@134.210.230.220>
2023-12-12 16:19:44 -0500

    Version 1.0 of TylerSort including Bubble, Selection, Merge, Shell, Quick and Heap sorts

--- ProgrammingTest/Projects/sorting/TestSort.java
+++ ProgrammingTest/Projects/sorting/TestSort.java
@@ -1,39 +1,41 @@
 package sorting;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
 public class TestSort
 {
-	
+
 	public static void main(String[] args)
 	{
-		//ArrayList<Integer> arr = new ArrayList<Integer>(Arrays.asList(9,8,7,6,5,4,3,2,1));
-		//ArrayList<Integer> arr = new ArrayList<Integer>(Arrays.asList(6,8,7,9,1,3,2,4,5));
 		ArrayList<Integer> arr = new ArrayList<Integer>();
 		TylerSort ts = new TylerSort();
-		Random rand = new Random();	
-		
-		int randomNumberSize = 1000000;
-		
-		for(int i = 0; i < randomNumberSize; i++)
+		Random rand = new Random();
+
+		int randomNumberSize = 20000;
+
+		for (int x = 1; x <= 10; x++)
 		{
-			arr.add(Math.abs(i-100));
-			arr.add(Math.abs(rand.nextInt() % 1000));
+			arr.clear();
+			for (int i = 0; i < randomNumberSize * x; i++)
+			{
+				// arr.add(Math.abs(i-100));
+				arr.add(Math.abs(rand.nextInt() % 1000));
+			}
+
+			System.out.println("Array filled with " + randomNumberSize * x + " random numbers.");
+
+			long timer = System.currentTimeMillis();
+
+			ts.bubbleSort(arr);
+
+			timer = System.currentTimeMillis() - timer;
+
+			System.out.println("Milliseconds to sort : " + timer);
+			System.out.println("Sorting Status : " + ts.validate(arr));
+			System.out.println("----------------------------------------");
 		}
-		
-		System.out.println("Array filled with " + randomNumberSize + " random numbers.");
-		
-		long timer = System.currentTimeMillis();
-		
-		ts.heapSort(arr);
-		//ts.quickSort(arr, 0, arr.size() - 1);
-		
-		timer = System.currentTimeMillis() - timer;
-		
-		System.out.println("Milliseconds to sort : "  + timer);
-		System.out.println("Sorting Status : " + ts.validate(arr));
-		
-		
+
 	}
 }
--- ProgrammingTest/Projects/sorting/TylerSort.java
+++ ProgrammingTest/Projects/sorting/TylerSort.java
@@ -1,19 +1,19 @@
 package sorting;
+
 import java.util.Random;
 
 import java.util.ArrayList;
 
-
 public class TylerSort
 {
 	Random rand;
-	
-	public TylerSort() 
+
+	public TylerSort()
 	{
 		rand = new Random();
 	}
-	
-	public ArrayList<Integer> bubbleSort(ArrayList<Integer> arr)
+
+	public void bubbleSort(ArrayList<Integer> arr)
 	{
 		boolean loop = true;
 		boolean swap = false;
@@ -38,10 +38,9 @@ public class TylerSort
 			}
 
 		}
-		return arr;
 	}
 
-	public ArrayList<Integer> selectionSort(ArrayList<Integer> arr)
+	public void selectionSort(ArrayList<Integer> arr)
 	{
 		boolean loop = true;
 		int position = 0;
@@ -49,13 +48,13 @@ public class TylerSort
 		int lowest = 0;
 		boolean swap = false;
 
-		while(loop)
+		while (loop)
 		{
-			
+
 			if (position < arr.size())
 			{
 				lowest = arr.get(position);
-			
+
 				for (int i = position; i < arr.size(); i++)
 				{
 					if (arr.get(i) < lowest)
@@ -77,10 +76,9 @@ public class TylerSort
 				loop = false;
 			}
 		}
-		return arr;
 	}
-		
-	public ArrayList<Integer> insertionSort(ArrayList<Integer> arr)
+
+	public void insertionSort(ArrayList<Integer> arr)
 	{
 		int size = arr.size();
 		for (int i = 1; i < size; i++)
@@ -95,10 +93,9 @@ public class TylerSort
 			}
 			arr.set(x + 1, temp);
 		}
-		return arr;
 	}
 
-	public ArrayList<Integer> mergeSort(ArrayList<Integer> arr)
+	public void mergeSort(ArrayList<Integer> arr)
 	{
 		if (arr.size() > 1)
 		{
@@ -150,127 +147,158 @@ public class TylerSort
 				r++;
 			}
 		}
-		return arr;
 	}
 
-	public ArrayList<Integer> shellSort(ArrayList<Integer> arr) // written from blackboard provided psudocode on tutorialspoint.com
+	public void shellSort(ArrayList<Integer> arr) // written from blackboard provided psudocode on tutorialspoint.com
 	{
 		int interval = 0;
 		int valueToInsert = -1;
 		int inner = -1;
-		
-		while(interval < arr.size()/3) // 0 1 3 10 31 64
+
+		while (interval < arr.size() / 3) // 0 1 3 10 31 64
 		{
 			interval = interval * 3 + 1;
 		}
-		
-		while(interval > 0) 
+
+		while (interval > 0)
 		{
-			for(int outer = interval; outer < arr.size(); outer++) 
+			for (int outer = interval; outer < arr.size(); outer++)
 			{
 				valueToInsert = arr.get(outer);
 				inner = outer;
-				
-				while(inner > interval - 1 && arr.get(inner - interval) >= valueToInsert) 
+
+				while (inner > interval - 1 && arr.get(inner - interval) >= valueToInsert)
 				{
 					arr.set(inner, arr.get(inner - interval));
 					inner -= interval;
 				}
-				
+
 				arr.set(inner, valueToInsert);
 			}
-			
-			interval = (interval - 1) / 3; 		
+
+			interval = (interval - 1) / 3;
 		}
-		return arr;
 	}
 
-	public ArrayList<Integer> quickSort(ArrayList<Integer> arr, int first, int last) 
-	{	
-		if(first < last) 
+	public void quickSort(ArrayList<Integer> arr) // alternate method to use same input format as other sorting algorithms
+	{
+		int first = 0;
+		int last = arr.size()-1;
+		
+		if (first < last)
 		{
 			int pivIndex = partition(arr, first, last);
 			quickSort(arr, first, pivIndex - 1);
 			quickSort(arr, pivIndex + 1, last);
 		}
-		return arr;
+
 	}
 	
-	public int partition(ArrayList<Integer> arr, int first, int last) //used for quick sort seperations
+	public void quickSort(ArrayList<Integer> arr, int first, int last) // written from book page 409
+	{
+		if (first < last)
+		{
+			int pivIndex = partition(arr, first, last);
+			quickSort(arr, first, pivIndex - 1);
+			quickSort(arr, pivIndex + 1, last);
+		}
+
+	}
+
+	public int partition(ArrayList<Integer> arr, int first, int last) // used for quick sort seperations
 	{
 		int pivot = arr.get(first);
 		int up = first;
 		int down = last;
-		
-		do{
-			while(up < last && pivot >= arr.get(up)) 
-			{ 
+
+		do
+		{
+			while (up < last && pivot >= arr.get(up))
+			{
 				up++;
 			}
-			
-			while(pivot < arr.get(down)) 
-			{ 
+
+			while (pivot < arr.get(down))
+			{
 				down--;
 			}
-			
-			if(up < down) 
+
+			if (up < down)
 			{
 				int temp = arr.get(up);
 				arr.set(up, arr.get(down));
 				arr.set(down, temp);
 			}
-		}
-		while(up < down);
+		} while (up < down);
 		{
 			int temp = arr.get(first);
 			arr.set(first, arr.get(down));
 			arr.set(down, temp);
 		}
-			
+
 		return down;
 	}
-	
-	ArrayList<Integer> heapSort(ArrayList<Integer> arr)
+
+	public void heapSort(ArrayList<Integer> arr)// based on the algorithm by Michael Sambol https://youtu.be/2DmK_H7IdTo also used the book and https://www.mygreatlearning.com/blog/heap-sort/ for deeper understanding
 	{
-//		1. Insert each value from the array to be sorted into a priority queue (heap).
-		arr = convertToHeap(arr);
-//		2. Set i to 0.
-		int i = 0;
-//		3. while the priority queue is not empty
-		while(arr.size() > 0) 
+		//turning array into heap
+		int size = arr.size();
+
+		for (int i = size/2 - 1; i >= 0; i--)
 		{
-//		4. Remove an item from the queue and insert it back into the array at position i.
-//		5. Increment i.
+			heapify(arr, size, i);
 		}
 		
-		return arr;
-	}
-	
-	public ArrayList<Integer> convertToHeap(ArrayList<Integer> arr)
-	{
-		for(int i = 0; i < arr.size(); i++) 
+		//sorting using heapify
+		for(int i = size -1; i >= 0; i--) 
 		{
-			
+			//swap with "root"
+			int temp = arr.get(i);
+			arr.set(i, arr.get(0));
+			arr.set(0, temp);
+			//double check root because it got swapped after sorting last element
+			heapify(arr, i, 0);
 		}
-			
 		
-		ArrayList<Integer> heap = new ArrayList<Integer>();
-		arr = heap;
-		return arr;
 	}
-	
-	public boolean validate(ArrayList<Integer> arr) 
-	{	
-		for(int i = 0; i < arr.size()-1; i++) 
+
+	public void heapify(ArrayList<Integer> arr, int size, int i) 
+	{
+		// these are the "Nodes" of the binary tree
+		int leftNode = 2 * i; //formula for children of parent
+		int rightNode = (2 * i) + 1; 
+		int largest = i; //root node
+
+		if (size > leftNode && arr.get(leftNode) > arr.get(largest))
 		{
-			if(arr.get(i) > arr.get(i+1)) 
+			largest = leftNode;
+		}
+		if (size > rightNode && arr.get(rightNode) > arr.get(largest)) //double check
+		{
+			largest = rightNode;
+		}
+		if (largest != i)
+		{
+			int temp = arr.get(i);
+			arr.set(i , arr.get(largest));
+			arr.set(largest, temp);
+			
+			heapify(arr, size, largest);
+		}
+	}
+
+	public boolean validate(ArrayList<Integer> arr)
+	{
+		for (int i = 0; i < arr.size() - 1; i++)
+		{
+			if (arr.get(i) > arr.get(i + 1))
 			{
 				return false;
 			}
 		}
 		return true;
 	}
-	
+
 	public void print(ArrayList<Integer> arr)
 	{
 		if (arr.size() > 0)
